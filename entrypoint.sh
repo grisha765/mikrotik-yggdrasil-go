f7c811b4649efdb7b9620d6646b1aeb8eb66fd5c
Grisha <grisha765@tuta.io>
2024-12-25 22:42:14 +0300

    [u] add env & use tun

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#set -euxo pipefail
+PEERS="${PEERS:-}"
 
 conf_file=/yggdrasil.conf
 
 [ -f "${conf_file}" ] || yggdrasil -genconf >${conf_file}
 
-sed -i 's/IfName: auto/IfName: none/g' ${conf_file}
-sed -i 's/AdminListen: unix:\/\/\/var\/run\/yggdrasil.sock/AdminListen: none/g' ${conf_file}
-sed -i 's/Listen: \[\]/Listen: \[ "tcp:\/\/0.0.0.0:3434" \]/g' ${conf_file}
+sed -i "/Peers: \[.*\]/c\  Peers: [ ${PEERS} ]" ${conf_file}
 
 exec /usr/bin/yggdrasil -useconffile ${conf_file}
